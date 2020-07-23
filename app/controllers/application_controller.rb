8be704ec371532a3b5e39eb9cbc78e8b316356f7
redhoshi <65010701+redhoshi@users.noreply.github.com>
2020-07-23 16:50:37 +0900

    song_db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+    def hello
+        #render html: "hello, world!"
+        #messageでparamsの引数を受け取る
+        @song = Song.all
+        render json: @song
+    end
 end
