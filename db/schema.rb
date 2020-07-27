5b6e43b39b4fecba3a1256e662112dd7d222ac85
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-07-27 11:56:45 +0900

    fix user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_021804) do
+ActiveRecord::Schema.define(version: 2020_07_27_024735) do
 
   create_table "songs", force: :cascade do |t|
     t.integer "rank_num"
@@ -21,9 +21,10 @@ ActiveRecord::Schema.define(version: 2020_07_24_021804) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "spotify_id"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "spotify_id"
   end
 
 end
