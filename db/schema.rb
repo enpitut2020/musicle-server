48fd58094a9da12e7f65f7963b8d2c671d8883fe
YoshidaAyane <s1811446@s.tsukuba.ac.jp>
2020-07-27 11:06:31 +0900

    gettingusser

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_071438) do
+ActiveRecord::Schema.define(version: 2020_07_24_021804) do
 
   create_table "songs", force: :cascade do |t|
     t.integer "rank_num"
     t.string "song_name"
     t.string "artist_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "spotify_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
