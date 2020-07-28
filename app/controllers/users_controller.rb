30b713228f2049387aa331baee90421bc40a220d
YoshidaAyane <s1811446@s.tsukuba.ac.jp>
2020-07-28 11:46:36 +0900

    displayname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def search
     #songs = UserSongRank.where(user_id: params[:id]).includes(:user_song)
+    name = "redshiho"
     ranks = UserSongRank.order(:rank_num).limit(5).where(user_id: params[:id]).includes(:user_song)
     songs_data = ranks.map {|rank| {name: rank.user_song.song_name, artistName: rank.user_song.artist_name} }
-    render json: songs_data
+    render json: {name: name, topSongs: songs_data}
   end
 end
