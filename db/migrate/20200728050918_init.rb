36ccb3d119b57dfece37eb84ea7da5734978f6a7
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-07-28 14:23:08 +0900

    add migration file

---
+++ db/migrate/20200728050918_init.rb
@@ -0,0 +1,25 @@
+class Init < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :spotify_id
+
+      t.timestamps
+    end
+
+    create_table :user_songs do |t|
+      t.string :spotify_id
+      t.string :song_name
+      t.string :artist_name
+
+      t.timestamps
+    end
+
+    create_table :user_song_ranks do |t|
+      t.integer :user_id
+      t.integer :rank_num
+      t.references :user_song
+      
+      t.timestamps
+    end
+  end
+end
