a998c306f46341eb04345098e4a6cef74e9259ec
Yuiki <10101661+Yuiki@users.noreply.github.com>
2020-07-28 12:19:12 +0900

    fix schema entirely

--- db/migrate/20200727024735_change_users.rb
+++
@@ -1,7 +0,0 @@
-class ChangeUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :name, :string
-    remove_column :users, :email, :string
-    add_column :users, :spotify_id, :string
-  end
-end
--- db/migrate/20200727061648_create_user_song_ranks.rb
+++
@@ -1,5 +0,0 @@
-class CreateUserSongRanks < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :user_song_ranks, :song_id, :user_song_id
-  end
-end
